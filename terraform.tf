ab1464ea03d0024e50de3c80313beb1b63ba789d
Bart Bogaerts <bart_bogaerts@be.ibm.com>
2025-11-25 21:21:18 +0100

    Change AWS region and add credentials
    Updated AWS region and added access/secret keys.

--- terraform.tf
+++ terraform.tf
@@ -8,5 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
+  access_key = "AKIAXYKJTDP376DCVUFV"
+  secret_key = "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"
 }
